fddd5a1f6a63bc39f6eb90821fc14de4ad177c4f
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2020-11-08 13:45:25 -0700

    added functions needed in js

---
+++ index.js
@@ -0,0 +1,46 @@
+function userClicksStart() {
+    //listen for user to click Start button
+    //when button is clicked, hide the button and load first question
+}
+
+function loadQuestion () {
+    //load the next question in the question array
+}
+
+function loadAnswerChoices () {
+    // gather answers that are associated with the relative question
+    //load the 4 answer choices as list items
+}
+
+function submitAnswer () {
+    //listen for user to click submit button
+    //if answer matches, display answer is correct
+    //if the answer is wrong, display the correct answer
+    //hide submit button, load next button
+}
+
+function nextQuestion () {
+    //listen for use to click next button
+    //run loadQuestion function
+}
+
+function results () {
+    //display the number of correct answers
+    //display restart quiz button
+}
+
+function restartQuiz () {
+    //listen for user to click restart button
+    //when button is clicked, load first question/answer
+    //clear score and question number
+}
+
+function currentScore () {
+    //if user gets questions correct, add one to the score
+    //update total score with number of questions submitted
+}
+
+function currentQuestion () {
+    //start at 1 out of length of questions
+    //add one each time user clicks next
+}
